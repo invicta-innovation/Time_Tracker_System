ae3ef906892bce6746845b0789d6e6629d2dba42
Thanan_Pathman <kerthy92@yahoo.com>
2019-10-15 15:41:02 +0530

    Conflict test 1

--- server/employee-api/src/app.module.ts
+++ server/employee-api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { RecruitmentTypesModule } from './recruitment-types/recruitment-types.module';
 import config from "./config/config"
+
+import { RecruitmentTypesModule } from './recruitment-types/recruitment-types.module';
 import { DesignationModule } from './designation/designation.module';
 
 @Module({
