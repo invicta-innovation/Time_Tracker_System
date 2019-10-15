78e214945e55c45802aaacbc2c2127d0cff2057a
Thanan_Pathman <kerthy92@yahoo.com>
2019-10-15 15:39:33 +0530

    Designation module moved to employee-api

--- server/employee-api/src/app.module.ts
+++ server/employee-api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RecruitmentTypesModule } from './recruitment-types/recruitment-types.module';
 import config from "./config/config"
+import { DesignationModule } from './designation/designation.module';
 
 @Module({
-  imports: [RecruitmentTypesModule,config]
+  imports: [RecruitmentTypesModule,config, DesignationModule]
 })
 export class AppModule {}
