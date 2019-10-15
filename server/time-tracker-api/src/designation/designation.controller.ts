02ee5f18bbf8effc3f129a28262545f6b8f773e6
Thanan_Pathman <kerthy92@yahoo.com>
2019-10-15 15:16:42 +0530

    designation module added. functionality development in progress

---
+++ server/time-tracker-api/src/designation/designation.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Req, Res, Post, Body, } from '@nestjs/common';
+import { DesignationDto } from './designation.dto';
+
+@Controller('designation')
+export class DesignationController {
+    @Get()
+    findAll(@Req() req: Request, @Res() res: Response) {
+        // return 'Please Replace this with something'
+        console.log(req.url)
+    }
+
+    @Post()
+    create(@Body() designationDto: DesignationDto): string {
+        return `Age: ${designationDto.designation}`;
+    }
+
+}
