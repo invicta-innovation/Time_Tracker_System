5108b8e65b162908c161b74a049cdb24cc34e8a6
jananthan93 <jjananthan93@gmail.com>
2019-10-15 17:28:27 +0530

    crud api done for recruitment type

--- server/employee-api/src/designation/designation.controller.ts
+++ server/employee-api/src/designation/designation.controller.ts
@@ -3,15 +3,15 @@ import { DesignationDto } from './designation.dto';
 
 @Controller('designation')
 export class DesignationController {
-    @Get()
-    findAll(@Req() req: Request, @Res() res: Response) {
-        // return 'Please Replace this with something'
-        console.log(req.url)
-    }
+    // @Get()
+    // findAll(@Req() req: Request, @Res() res: Response) {
+    //     // return 'Please Replace this with something'
+    //     console.log(req.url)
+    // }
 
-    @Post()
-    create(@Body() designationDto: DesignationDto): string {
-        return `Age: ${designationDto.designation}`;
-    }
+    // @Post()
+    // create(@Body() designationDto: DesignationDto): string {
+    //     return `Age: ${designationDto.designation}`;
+    // }
 
 }
