5bdf7b87f8c0a0a48c064a95ce4435d7dcc64f7c
jananthan93 <jjananthan93@gmail.com>
2019-10-18 08:49:08 +0530

    timesheet inline editing woked ui done

--- client/src/views/timetracker/timesheet/taskList.js
+++ client/src/views/timetracker/timesheet/taskList.js
@@ -9,26 +9,25 @@ const tasks = [
     taskName: "request leave",
     description: "apply for a leave",
     duration: 3.0
+  },
+  {
+    id: 2,
+    projectName: "leave",
+    taskName: "request leave",
+    description: "apply leave",
+    duration: 2.5
   }
 ];
 const c = 1;
 class DailyTask extends Component {
   state = {
-    edit: false,
     isAdd: false
   };
   handlePush = taskObj => {
     tasks.push(taskObj);
     this.setState({ isAdd: false });
   };
-  handleSubmit = taskObj => {
-    tasks.map(task => {
-      if (task.id === taskObj.id) {
-        return taskObj;
-      }
-    });
-    this.setState({ edit: false });
-  };
+  
   render() {
     return (
       <div>
@@ -42,18 +41,11 @@ class DailyTask extends Component {
             </tr>
           </thead>
           <tbody>
-            {tasks.map(task =>
-              (!this.state.edit) ? (
+            {tasks.map(task =>(
                 <TaskRow
+                  tasks={tasks}
                   key={task.id}
                   task={task}
-                  handleEdit={() => this.setState({ edit: true })}
-                />
-              ) : (
-                <TaskForm
-                  key={task.id}
-                  task={task}
-                  handleSubmit={obj => this.handleSubmit(obj)}
                 />
               )
             )}
--- client/src/views/timetracker/timesheet/taskRow.js
+++ client/src/views/timetracker/timesheet/taskRow.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
+import TaskForm from "./taskForm";
 
 export default class TaskRow extends Component {
+  state={
+    edit:false
+  }
+  handleSubmit = taskObj => {
+    this.props.tasks.map(task => {
+      if (task.id === taskObj.id) {
+        return taskObj;
+      }else{
+        return task;
+      }
+    });
+    this.setState({ edit: false });
+  };
   render() {
     const task = this.props.task;
     return (
       <>
-        <tr>
-          <td>{task.projectName}</td>
-          <td>{task.taskName}</td>
-          <td>{task.description}</td>
-          <td>{task.duration}</td>
-          <td>
-            <button 
-             type="submit"
-              class="btn btn-info " 
-              onClick={this.props.handleEdit}>Edit</button>
-          </td>
-        </tr>
+        {!this.state.edit ? (
+          <tr>
+            <td>{task.projectName}</td>
+            <td>{task.taskName}</td>
+            <td>{task.description}</td>
+            <td>{task.duration}</td>
+            <td>
+              <button
+                type="submit"
+                class="btn btn-info "
+                onClick={() => this.setState({ edit: true })}
+              >
+                Edit
+              </button>
+            </td>
+          </tr>
+        ) : (
+          <TaskForm
+            key={task.id}
+            task={task}
+            handleSubmit={obj => this.handleSubmit(obj)}
+          />
+        )}
       </>
     );
   }
